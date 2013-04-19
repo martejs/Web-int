ced36cef8d7cc9316ce670b27386b1b4fb34897a
Marte Sæther <martesather@gmail.com>
2013-04-19 15:39:58 +0200

    hæ?

---
+++ Web-intelligent/src/Stemming/XMLParse.java
@@ -0,0 +1,157 @@
+package Stemming;
+
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
+public class XMLParse {
+
+	private int i = 0;
+	private boolean isFileMade = false;
+	private File oldFile = new File("tokenized.xml");
+	htmlstripper hs ;
+	private boolean fromUrl;
+
+	/**
+	 * konstruktoren til klassen Den tar inn konfirurasjonsfila
+	 * @param file File configfilen
+	 *@param boolean fromUrl Sjekker om inputfilene skal komme fra fil eller fra nett
+	 */
+	public XMLParse(File file, boolean fromUrl){
+		this.fromUrl = fromUrl;
+		readXML(file);
+	}
+
+	/**
+	 * Metoden leser innholdet i konfigurasjonsfilen(xml filen)
+	 * @param file String
+	 */
+	public void readXML(File file){
+		SAXBuilder myBuilder = new SAXBuilder();
+		Document myDocument = null;
+		try {
+			myDocument = myBuilder.build(file);
+		}
+		catch (JDOMException jde) {
+			jde.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Element root = myDocument.getRootElement();
+		Element collection = root.getChild("collection");
+		Element uri = collection.getChild("uri");
+		Element directory = collection.getChild("directory");
+		Element tokenizer = root.getChild("tokenizer");
+		Element sentenceboundary = tokenizer.getChild("sentenceboundary");
+		Element paragraphboundary = tokenizer.getChild("paragraphboundary");
+		Element allsmall = tokenizer.getChild("allsmall");
+
+		String urls = uri.getText();
+		String directoryText = directory.getText();
+		String sBoundary = sentenceboundary.getText();
+		String pBoundary = paragraphboundary.getText();
+		String aSmall = allsmall.getText();
+
+		if(fromUrl){
+			ArrayList uriList = parseUri(urls);
+			for(int i = 0; i < uriList.size(); i++){
+				hs = new htmlstripper((String)uriList.get(i), sBoundary, pBoundary, aSmall, this);
+			}
+		}
+		else{
+			hs = new htmlstripper(directoryText, this, sBoundary, pBoundary, "" ); // Denne kjøres dersom det skal hentes fra fil
+		}
+	}
+
+
+	/**
+	 * Metoden finner alle urlene som ble tatt inn i konfigurasjonsfilen
+	 * @param urls String
+	 * @return ArrayList
+	 */
+	public ArrayList parseUri(String urls){
+		StringTokenizer st = new StringTokenizer(urls);
+		ArrayList uriList = new ArrayList();
+		while(st.hasMoreTokens()){
+			uriList.add(st.nextToken());
+		}
+		return uriList;
+	}
+
+
+	/**
+	 * Metoden lager tokenize.xml fila som skal sendes videre til med xml-fila
+	 * @param title String
+	 * @param url String
+	 * @param body String
+	 * @throws java.io.FileNotFoundException
+	 * @throws java.lang.SecurityException
+	 */
+	public void makeXmlTokenizer(String title, String url, String body, String name) throws FileNotFoundException, SecurityException{
+		String fileContent = "";
+		//		  try {
+		//				fileContent = readFile(oldFile);
+		//		  } catch (IOException e) {
+		//				e.printStackTrace();
+		//		  }
+		String encoding = "ISO-8859-1";
+		fileContent = fileContent.replaceAll("<\\?xml version=\"1.0\" encoding=\""+ encoding+"\"\\?>", "");
+		fileContent = fileContent.replaceAll("<tokenized>", "");
+		fileContent = fileContent.replaceAll("</tokenized>", "");
+		File file = new File(url);
+		String urlFolder = "";
+		if(file.isDirectory()){// && !file.getName().substring(0, 0).equals(name)){
+			urlFolder = "../Tokenized/" + url.charAt(0);		
+			boolean folder = (new File(urlFolder)).mkdirs();
+		}else{
+			urlFolder = "../Tokenized/" + name;
+			new File(urlFolder).mkdirs();
+		}
+		//		  PrintWriter out = new PrintWriter(new FileOutputStream("tokenized.xml"));
+
+		PrintWriter out = new PrintWriter(new FileOutputStream(urlFolder + "/" + url.substring(0, url.indexOf(".htm")) +".xml"));
+		out.println("<?xml version=\"1.0\" encoding=\""+ encoding+"\"?>");
+		out.println("<tokenized>");
+		out.println(fileContent);
+		out.println("<doc id = \"" + url + "\">");
+		out.println("<title>");
+		out.println(title);
+		out.println("</title>");
+		out.println("<body>");
+		out.println(body);
+		out.println("</body>");
+		out.println("</doc>");
+		out.println("</tokenized>");
+		out.flush();
+		out.close();
+	}
+
+
+	/**
+	 * Metoden leser innholdet i fila og lagrer det i en string
+	 * @param filename File
+	 * @return String
+	 * @throws java.io.IOException
+	 */
+	public String readFile(File filename) throws IOException{
+		BufferedReader in = new BufferedReader(new FileReader(filename));
+		String file = "";
+		String temp;
+		while((temp = in.readLine())!=null){
+			file+=temp;
+			file+=" ";
+		}
+		return file;
+	}
+}
--- Web-intelligent/src/Stemming/htmlstripper.java
+++ Web-intelligent/src/Stemming/htmlstripper.java
@@ -1,7 +1,156 @@
 package Stemming;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class htmlstripper {
 	
+	
+
+	private File [] files;
+	private String url;
+	private String orginalFile = "";
+	private String pBoundary = "" ;
+	private String sBoundary = "";
+	private XMLParse xmlP ;
+
+	/**
+	 * Konstruktoren som tar inn en url og som henter inn parametre fra konfigurasjonsfila
+	 * @param url String
+	 * @param sBoundary String
+	 * @param pBoundary String
+	 * @param allsmall String
+	 * @param xmlP XMLParser
+	 */
+	public htmlstripper(String url, String sBoundary, String pBoundary, String allsmall, XMLParse xmlP){
+		this.url = url;
+		this.pBoundary = pBoundary;
+		this.sBoundary = sBoundary;
+		this.xmlP = xmlP;
+		try {
+			orginalFile = findSource(url);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		parseFile(orginalFile, "");
+	}
+
+	/**
+	 * Konstruktoren som tar inn html fil fra fil
+	 * @param filename String
+	 * @param rootname String
+	 * @param url String
+	 */
+	public htmlstripper(String filename,   XMLParse xmlP, String sBoundary, String pBoundary, String name){
+		File file = new File(filename);
+		this.url = file.getName();
+		this.xmlP = xmlP;
+		this.pBoundary = pBoundary;
+		this.sBoundary = sBoundary;
+		if(file.isDirectory()){//sjekker om det sendes inn en katalog
+			System.out.println("Filen er en directory");
+			files = file.listFiles();//henter filene som er i den katalogen
+			for(int i = 0; i<files.length; i++){
+				new htmlstripper(files[i].getAbsolutePath(),  xmlP, sBoundary, pBoundary, file.getName());
+			}
+		}
+		else{//hvis ikke en katalog
+			System.out.println("Filen er en fil " +file.getName());
+			try {
+				orginalFile = readFile(file);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			parseFile(orginalFile, name);
+		}
+
+	}
+
+	/**Metode som henter ut metadata fra fil
+	 * Mulighet for utvidelse av system
+	 */
+	public ArrayList getMeta(String file){
+		ArrayList meta = new ArrayList();
+		int start = file.indexOf("meta name=\"keywords\" content=\"");
+		int slutt = file.indexOf("\">", start);
+		String keyWords = file.substring(start+30,slutt);
+		StringTokenizer st = new StringTokenizer(keyWords,",");
+		while(st.hasMoreTokens()){
+			meta.add(st.nextToken());
+		}
+		start = file.indexOf("<meta name=\"description\" content=\"");
+		slutt = file.indexOf("\">", start);
+		keyWords = file.substring(start+31,slutt);
+		StringTokenizer st2 = new StringTokenizer(keyWords,",");
+		while(st2.hasMoreTokens()){
+			meta.add(st.nextToken());
+		}
+		return meta;
+	}
+
+
+	/**
+	 * Denne metoden kaller alle de forskjellige metodene som parser file
+	 * @param file String
+	 */
+	public void parseFile(String file, String name){
+		String tagsRemoved = "";
+		ArrayList sourceList = searchFrame(file);
+		String ferdigFil = file.toLowerCase();
+		String title = searchTitle(ferdigFil);
+		String strippedFrame ;
+		orginalFile = ferdigFil;
+		tagsRemoved = strip(orginalFile);
+		tagsRemoved = stripWhiteSpace(tagsRemoved);
+		for(int i = 0; i< sourceList.size(); i++){
+			strippedFrame =parseFrame((String)sourceList.get(i));
+			tagsRemoved += strippedFrame;
+		}
+		try {
+			xmlP.makeXmlTokenizer(title, url,tagsRemoved, name);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	/**
+	 * Metoden kaller de metodene som må kalles for å parse innholdet i en frame
+	 * @param frame String
+	 * @return String
+	 */
+	private String parseFrame(String frame) {
+		String tagsRemovedFromeFrame  = "";
+		ArrayList sourceList = searchFrame(frame);
+		frame = frame.toLowerCase();
+		frame = letterStripping(frame);
+		frame = stripSpecialChar(frame);
+		tagsRemovedFromeFrame = strip(frame);
+		tagsRemovedFromeFrame = removeStopWords(tagsRemovedFromeFrame);
+		tagsRemovedFromeFrame = stripWhiteSpace(tagsRemovedFromeFrame);
+		//		  indexFile(tagsRemovedFromeFrame);
+		return tagsRemovedFromeFrame;
+	}
+
+
+
+	private void indexFile(String tagsRemovedFromeFrame) {
+		// TODO Auto-generated method stub
+
+	}
 	
 	
 	//hallo
@@ -29,5 +178,216 @@ public class htmlstripper {
 		
 		return fil;
 		
+	}
+	/**
+	 * Metoden leser filen og legger den i en string variabel
+	 * @param filename File
+	 * @return String
+	 * @throws java.io.IOException
+	 */
+	public String readFile(File filename) throws IOException{
+		BufferedReader in = new BufferedReader(new FileReader(filename));
+		String file = "";
+		String temp;
+		while((temp = in.readLine())!=null){
+			file+=temp;
+			file+=" ";
+		}
+		return file;
+	}
+
+
+	/**
+	 * Metoden finner tittlen til html siden
+	 * @param file String
+	 * @return String
+	 */
+	public String searchTitle(String file){
+		String title = "";
+		int start = file.indexOf("<title>");
+		int slutt = file.lastIndexOf("</title>");
+		if(start==-1||slutt==-1){
+			return title;
+		}
+		else{
+			title = file.substring(start+7,slutt);
+			title = letterStripping(title);
+			title = stripSpecialChar(title);
+			return title;
+		}
+	}
+
+
+	/**
+	 * Metoden finner eventuelle frames som er i html fila
+	 * @param file String
+	 * @return ArrayList
+	 */
+	public ArrayList searchFrame(String file){
+		if(file == null){
+			System.out.println("file er null");
+		}
+		String frame = "";
+		ArrayList frameList = new ArrayList();
+		file = file.replaceAll("\\<(FRAME|frame) (SRC|src) =\"(.*?)\".+?\\>", "§"+"$1"+ "§L" );
+		while((file.indexOf("§"))!= -1 && (file.indexOf("§L"))!= -1){
+			int first = file.indexOf("§");
+			int last = file.indexOf("§L");
+			System.out.println(file);
+			frame = file.substring(first+1, last);
+			file = file.replaceFirst("§" + frame + "§L", " ");
+			if(frame.startsWith("http")){
+				frameList.add(frame);
+			}
+			else{
+				frame = url + frame;
+				frameList.add(frame);
+			}
+		}
+		String source;
+		ArrayList sourceList = new ArrayList();
+		for(int i = 0; i < frameList.size(); i++){
+			try {
+				source	= findSource((String)frameList.get(i));
+				sourceList.add(source);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return sourceList;
+	}
+
+
+	/**
+	 * Metoden finner kildekoden til en url
+	 * @param urlForFrame String
+	 * @return String
+	 * @throws java.net.MalformedURLException
+	 * @throws java.io.IOException
+	 */
+	public String findSource(String urlForFrame) throws MalformedURLException,  IOException{
+		URL frameUrl = new URL(urlForFrame);
+		Object source = frameUrl.getContent();
+		InputStream is = (InputStream)source;
+		BufferedReader br = new BufferedReader(new InputStreamReader(frameUrl.openStream()));
+		String content = "";
+		String linje = br.readLine();
+		while(linje != null){
+			content = content.concat(linje);
+			linje = br.readLine();
+		}
+		return content;
+	}
+
+
+	/**
+	 * Metoden fjerner de fleste html tagger og mellomrom
+	 * @param file String
+	 * @return
+	 */
+	public String strip(String file){
+		file = file.replaceAll("(\\p{Space}){2,}", " ");
+		//file = file.replaceAll("\\s{2,}", " ");
+		file = file.replaceAll("<a href=\"http://(.*?)\".+?/a>", " "+"http://$1"+ " " );
+		file = file.replaceAll("<a href=\"#(.*?)\".+?/a>", " "+url+"#$1"+ " " );
+		file = file.replaceAll("<a href=\"(.*?)\".+?/a>", " "+url+"$1"+ " " );
+		file = file.replaceAll("(<script.+?</script>)+", "");
+
+		file = file.replaceAll("<p>", pBoundary);
+		file = file.replaceAll("<[^>]*>", " ");
+		file = file.replaceAll("</[^>]*>", " ");
+		file = file.replaceAll("(<meta.*>)+?","");
+
+		file = file.replaceAll("#.*\\{[^\\}]*\\}"," "); // tar stylesheet oppmerking
+		file = letterStripping(file);
+		file = stripSpecialChar(file);
+
+		file = file.replaceAll("mailto:(.*?)", "$1");
+
+		file =removeStopWords(file);
+
+		char[] tegn = {',', ';', '!'};
+		for(int i = 0; i < tegn.length ; i++){
+			file = file.replaceAll((String.valueOf(tegn[i])), sBoundary);
+		}
+
+		file = file.replaceAll("\"", " ");
+		file = file.replaceAll("\'", " ");
+		return file;
+	}
+
+
+	/**
+	 * Metoden fjerner spesialtegn som kan forekomme i html
+	 * @param file String
+	 * @return
+	 */
+	public String stripSpecialChar(String file){
+		file = file.replaceAll("&nbsp;", " ");
+		file = file.replaceAll("&lt;", " ");
+		file = file.replaceAll("&gt;", " ");
+		file = file.replaceAll("&copy;", " ");
+		file = file.replaceAll("&#[^;];"," ");
+		file = file.replaceAll("&#150", " ");//dette er bindestrek
+		file = file.replaceAll("&", " ");
+		file = file.replaceAll("#" , " ");
+		file = file.replaceAll("\\|", " ");
+		file = file.replaceAll("-", " ");
+		file = file.replaceAll("\"", " ");
+		file = file.replaceAll("\\(", " ");
+		file = file.replaceAll("\\)", " ");
+		return file;
+	}
+
+
+	/**
+	 * Metoden fjerner alle whiteSpaces som er overflødige
+	 * @param file String
+	 * @return String
+	 */
+	public String stripWhiteSpace(String file){
+		char[] liste = file.toCharArray();
+
+		for (int i = 0; i < (liste.length -1); i++){
+			if(Character.isWhitespace(liste[i])|| liste[i] == 'S' ){
+				if(Character.isWhitespace(liste[i+1])|| liste[i+1] == 'S'){
+					liste[i] = 'Ø';
+				}
+			}
+		}
+
+		String newfile = String.valueOf(liste );
+		newfile = newfile.replaceAll("Ø", "");
+		newfile = newfile.replaceAll("S", " ");
+		return newfile;
+	}
+
+
+	/**
+	 * Metoden gjør om bokstaver som har spesialtegn i html til vanlige bokstaver
+	 * @param file String
+	 * @return String
+	 */
+
+	public String letterStripping(String file){
+
+		
+		file = file.replaceAll("&aelig;", "æ");
+		
+
+		file = file.replaceAll("&aring;", "å");
+
+		return file;
+	}
+
+
+	/**
+	 * Main metoden som starter alt
+	 * @param args String
+	 */
+	public static void main(String [] args){
+		XMLParse xmlP = new XMLParse(new File("config.xml"), false);
 	}
 }
